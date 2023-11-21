3251343aadc800c00727a6b86b6669ab27b98b6f
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2023-11-21 09:40:21 +0100

    Initial commit

---
+++ data/faq.js
@@ -0,0 +1,192 @@
+const questions = [
+    {
+        category: "algemeen",
+        question: "Wat is Lightopia Brussels?",
+        answer: "Een meermaals georganiseerd lantaarn- en lichtfestival dat traditionele handgemaakte lantaarns combineert met modern design en technologie zoals een watershow, 3D mapping en artistieke installaties om een immersieve ervaring te creëren.",
+    },
+    {
+        category: "algemeen",
+        question: "Waar vindt Lightopia Brussels plaats?",
+        answer: "",
+    },
+    {
+        category: "algemeen",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "algemeen",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "algemeen",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "parcours",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "parcours",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "parcours",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "parcours",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "parcours",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "parcours",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "parcours",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "parcours",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "parcours",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "parcours",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+    {
+        category: "",
+        question: "",
+        answer: "",
+    },
+];
