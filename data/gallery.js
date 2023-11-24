5b7841b1001907145c91f910b151ca47d6d6e718
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2023-11-24 08:58:14 +0100

    Added gallery images in data folder

---
+++ data/gallery.js
@@ -0,0 +1,42 @@
+const gallery = [
+    {
+        imageLink: './static/img/galerij_img_1',
+        name: 'lightopia_afbeelding_1',
+    },
+    {
+        imageLink: './static/img/galerij_img_2',
+        name: 'lightopia_afbeelding_2',
+    },
+    {
+        imageLink: './static/img/galerij_img_3',
+        name: 'lightopia_afbeelding_3',
+    },
+    {
+        imageLink: './static/img/galerij_img_4',
+        name: 'lightopia_afbeelding_4',
+    },
+    {
+        imageLink: './static/img/galerij_img_5',
+        name: 'lightopia_afbeelding_5',
+    },
+    {
+        imageLink: './static/img/galerij_img_6',
+        name: 'lightopia_afbeelding_6',
+    },
+    {
+        imageLink: './static/img/galerij_img_7',
+        name: 'lightopia_afbeelding_7',
+    },
+    {
+        imageLink: './static/img/galerij_img_8',
+        name: 'lightopia_afbeelding_8',
+    },
+    {
+        imageLink: './static/img/galerij_img_9',
+        name: 'lightopia_afbeelding_9',
+    },
+    {
+        imageLink: './static/img/galerij_img_10',
+        name: 'lightopia_afbeelding_10',
+    },
+];
