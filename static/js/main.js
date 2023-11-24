3a2bf748c78689d688054818b51b6988b718841b
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2023-11-24 09:47:33 +0100

    Added gallery focus in js to select a photo

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
 const $galleryElement = document.querySelector('.gallery');
+const $galleryFocusElement = document.querySelector('.gallery-focus');
 
 // ---------------- GALLERY ------------------------------------------
 // Generate the HTML for the gallery
@@ -11,6 +12,39 @@ function generateHTMLForGallery (photos) {
 // Show the gallery
 function generateUIForGallery () {
     $galleryElement.innerHTML = generateHTMLForGallery(gallery);
+    // If the user clicks on a photo show the gallery-focus (photo in big)
+    const $elems = $galleryElement.querySelectorAll('.gallery__photo');
+    $elems.forEach($elem => {
+        $elem.addEventListener('click', (ev) => {
+            const id = ev.currentTarget.dataset.id; 
+            generateUIForGalleryFocus(id);
+            // If the user clicks on the remove button -> class .concert-details--open is removed and closes the concert-details
+            document.querySelector('.remove').addEventListener('click', () => {
+                $galleryFocusElement.classList.remove('gallery-focus--open')
+            })
+        })
+    });
+}
+
+// Generate the HTML for gallery-focus
+function generateHTMLForGalleryFocus (photo) {
+    return `
+    <div class="gallery-focus__inner">
+        <div class="button-container">
+            <button class="remove">x</button>
+        </div>
+        <div class="photo-container">
+            <img class="gallery__photo" src="${photo.imageLink}" alt="${photo.name}">
+        </div>
+    </div>
+    `;
+}
+
+// Show the gallery-focus
+function generateUIForGalleryFocus (id) {
+    const photo = gallery.find((photo) => photo.id === id);
+    $galleryFocusElement.classList.add('gallery-focus--open')
+    $galleryFocusElement.innerHTML = generateHTMLForGalleryFocus(photo);
 }
 
 // ---------------- GENERATE USER INTERFACE ------------------------------------------
