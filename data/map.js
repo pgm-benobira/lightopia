ae22ffa48a75e01181e61695e2a235feac54a123
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2023-12-05 14:34:33 +0100

    Generating map pointers and their position

---
+++ data/map.js
@@ -0,0 +1,32 @@
+const markers = [
+    {
+        name: 'Map image 1',
+        imageLink: './static/img/map-images/01.jpg',
+        y: '60.4%',
+        x: '68%',
+    },
+    {
+        name: 'Map image 2',
+        imageLink: './static/img/map-images/02.jpg',
+        y: '48%',
+        x: '56.5%',
+    },
+    {
+        name: 'Map image 3',
+        imageLink: './static/img/map-images/03.jpg',
+        y: '45%',
+        x: '41%',
+    },
+    {
+        name: 'Map image 4',
+        imageLink: './static/img/map-images/04.jpg',
+        y: '24%',
+        x: '37%',
+    },
+    {
+        name: 'Map image 5',
+        imageLink: './static/img/map-images/05.jpg',
+        y: '29%',
+        x: '15%',
+    },
+];
